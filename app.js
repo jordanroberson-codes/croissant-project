b1d193be1aca63cd4196b02f314aefc0e9a34b37
Jordan Roberson <jordanrobersonnp@gmail.com>
2021-10-04 12:01:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function submitClick() {
-  alert("Thanks for submiting!");
+  alert("Thanks for submiting! We will reach out to you shortly!");
 }
 let submit = document.querySelector("#submit-btn");
 submit.addEventListener("click", submitClick);
